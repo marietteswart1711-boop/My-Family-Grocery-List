9fa46f21d8aa517ea275e5c0bc26a20d1d1e7dcd
marietteswart1711-boop <marietteswart1711@gmail.com>
2026-06-22 13:51:36 +0200

    Add files via upload

---
+++ family-sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'family-grocery-v2';
+const FILES = [
+  './family-grocery-list.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./family-grocery-list.html')))
+  );
+});
